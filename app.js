dea4b3645d7b135f7f473bdd25f141e2285cea09
zm412 <zm412412@gmail.com>
2022-12-03 18:10:18 -0500

    add app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const router = require("./router.js" )
+const nodeInterface = require("./nodeInterface.js" )
+
+const app = express()
+
+app.use(express.json())
+app.use(express.static('static'))
+app.use('/api', router)
+
+async function main() {
+    //const url = "mongodb+srv://name:passw@cluster0.589kf.mongodb.net"
+    await mongoose.connect(url)
+}
+
+const nodeArgs = process.argv.slice(2)
+if(nodeArgs.length > 0){
+    nodeInterface(nodeArgs)
+}
+
+
+main()
+// прослушиваем прерывание работы программы (ctrl-c)
+process.on("SIGINT", async() => {
+
+    await mongoose.disconnect()
+    console.log("Приложение завершило работу")
+    process.exit()
+});
+
+main().catch(console.log)
+app.listen(3000)
